aaccb9fd668bdb136fe47aa8ceac445f93f34215
Gustavo Barbosa <gustavocsb@gmail.com>
2019-05-20 17:24:07 -0400

    components: add timeline progress

---
+++ lib/src/components/Timeline/index.ts
@@ -0,0 +1,1 @@
+export { default as TimelineProgress } from './TimelineProgress'
--- lib/src/components/index.ts
+++ lib/src/components/index.ts
@@ -1,3 +1,4 @@
 export * from './Banners'
 export * from './Buttons'
 export * from './TextInputs'
+export * from './Timeline'
