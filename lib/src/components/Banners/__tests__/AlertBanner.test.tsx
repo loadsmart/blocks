0f1358b7753d1fa8aa9a9361c34cf4d7cb6efbc2
Gustavo Barbosa <gustavocsb@gmail.com>
2019-05-20 12:03:19 -0400

    banner: add tests

--- lib/src/components/Banners/__tests__/AlertBanner.test.tsx
+++ lib/src/components/Banners/__tests__/AlertBanner.test.tsx
@@ -1,11 +1,46 @@
+import React from 'react'
+import renderer from 'react-test-renderer'
+import AlertBanner from '../AlertBanner'
+import { Image, Text, TouchableWithoutFeedback } from 'react-native'
+import { Images } from '../../../res/'
+import { BannerDisplayStyle } from '../BannerProps'
+
 describe('AlertBanner', () => {
-  it('has an icon', () => {})
+  it('has an icon', () => {
+    const testRenderer = renderer.create(<AlertBanner message={'hello'} />)
+    const icon = testRenderer.root.findByType(Image)
+    expect(icon.props.source).toBe(Images.Warning)
+  })
 
-  it('has the title I passed', () => {})
+  it('has the title I passed', () => {
+    const testRenderer = renderer.create(<AlertBanner message={'hello'} />)
+    const title = testRenderer.root.findByType(Text)
+    expect(title.props.children).toBe('hello')
+  })
 
-  it('renders for warning style', () => {})
+  it('renders for warning style', () => {
+    const testRenderer = renderer.create(
+      <AlertBanner message={'hello'} displayStyle={BannerDisplayStyle.Warning} />
+    )
+    expect(testRenderer.toJSON()).toMatchSnapshot()
+  })
 
-  it('renders for warning style', () => {})
+  it('renders for error style', () => {
+    const testRenderer = renderer.create(
+      <AlertBanner message={'hello'} displayStyle={BannerDisplayStyle.Error} />
+    )
+    expect(testRenderer.toJSON()).toMatchSnapshot()
+  })
 
-  it('executes onPress callback', () => {})
+  it('executes onPress callback', () => {
+    const onPressCallback = jest.fn()
+    const testRenderer = renderer.create(
+      <AlertBanner message={'hello'} onPress={onPressCallback} />
+    )
+
+    const touchable = testRenderer.root.findByType(TouchableWithoutFeedback)
+    touchable.props.onPress()
+
+    expect(onPressCallback).toBeCalledTimes(1)
+  })
 })
