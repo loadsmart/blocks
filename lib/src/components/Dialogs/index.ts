571ca8b6361569114c3940d396f4c07cc7a27408
Gustavo Barbosa <gustavocsb@gmail.com>
2019-06-11 19:47:04 -0400

    dialogs: add wrapper component to trigger modal opening

--- lib/src/components/Dialogs/index.ts
+++ lib/src/components/Dialogs/index.ts
@@ -1,2 +1,3 @@
 export { default as Dialog } from './Dialog'
 export { default as DialogBox } from './DialogBox'
+export * from './DismissibleModalProps'
---
+++ src/components/Dialogs/helpers.ts
@@ -0,0 +1,4 @@
+export interface Props {}
+export interface State {
+  modalVisible: boolean
+}
