a0bc654b265ca2e5b2a3a5250695cf71114a005f
Lenoir Zamboni <lenoir.zamboni@loadsmart.com>
2019-09-06 14:09:42 -0300

    components: add password text input

--- src/res/images.ts
+++ src/res/images.ts
@@ -3,6 +3,8 @@ const Images = {
   PlusSign: require(`../../assets/images/plus-sign.png`),
   SmallCheck: require(`../../assets/images/small-check.png`),
   Warning: require(`../../assets/images/warning.png`),
+  PasswordShow: require('../../assets/images/password-show.png'),
+  PasswordHide: require('../../assets/images/password-hide.png'),
 }
 
 export default Images
